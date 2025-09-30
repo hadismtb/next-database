0a367fc4db93d3f8d899f02341a1a24b7c5c124a
Hadis <hadisematloob@yahoo.com>
2025-09-30 11:15:21 +0330

    feat(post-name): get name from user and send to server with api

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
 import { Inter } from 'next/font/google'
+import {useState} from "react";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [name, setName] = useState("");
+
+  const postHandler = async () => {
+    const res = await fetch("/api/data", {
+      method: "POST",
+      body: JSON.stringify({name}),
+      headers: {"Content-Type": "application/json"}
+    });
+
+    const data = await res.json();
+    console.log(data);
+  }
+
   return (
-    <h1>Data Base</h1>
+    <div>
+      <h1>Data Base</h1>
+      <div>
+        <input placeholder="Eter name" value={name} onChange={e => setName(e.target.value)} />
+        <button onClick={postHandler}>Post</button>
+      </div>
+    </div>
   )
 }
